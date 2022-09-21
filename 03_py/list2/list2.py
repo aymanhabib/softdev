ca595e9eacb826a785fb0d2525f5f8349baf7171
aymanhabib <109638308+aymanhabib@users.noreply.github.com>
2022-09-21 01:56:24 -0400

    Update list2.py

--- 03_py/list2/list2.py
+++ 03_py/list2/list2.py
@@ -16,3 +16,41 @@ def centered_average(nums):
     sum += x
   return sum / len(nums)
 
+def sum13(nums):
+  sum = 0
+  for i in range(len(nums)):
+    if nums[i] == 13:
+      sum += 0
+    elif i == 0 and nums[0] != 13:
+      sum += nums[0]
+    elif nums[i-1] == 13:
+      sum += 0
+    else:
+      sum += nums[i]
+  return sum
+
+def sum67(nums):
+    record = True
+    total = 0
+
+    for n in nums:
+        if n == 6:
+            record = False
+
+        if record:
+            total += n
+            continue
+
+        if n == 7:
+            record = True
+
+    return total
+
+def has22(nums):
+    for i in range(len(nums) - 1):
+        if nums[i] == 2 and nums[i + 1] == 2:
+            return True
+
+    return False
+
+
