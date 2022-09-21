cf45f1fc31bf0b18f7ae000a07186522ffa4b2f7
aymanhabib <109638308+aymanhabib@users.noreply.github.com>
2022-09-21 01:54:00 -0400

    Update list1.py

--- 03_py/list1/list1.py
+++ 03_py/list1/list1.py
@@ -17,3 +17,39 @@ def sum3(nums):
     sum += x
   return sum
 
+def rotate_left3(nums):
+  first = nums[0]
+  last = nums[-1]
+  mid = nums[1]
+  rotArr = [mid, last, first]
+  return rotArr
+  
+def reverse3(nums):
+  return [nums[-1], nums[1], nums[0]]
+
+def max_end3(nums):
+  if nums[-1] > nums[0]:
+    return [nums[-1], nums[-1], nums[-1]]
+  else:
+    return [nums[0], nums[0], nums[0]]
+    
+def sum2(nums):
+  if len(nums) >= 2:
+    return nums[0] + nums[1]
+  elif len(nums) == 1:
+    return nums[0]
+  else:
+    return 0
+    
+def middle_way(a, b):
+  return [a[1], b[1]]
+  
+def make_ends(nums):
+  return [nums[0], nums[-1]]
+
+def has23(nums):
+  if nums[0] == 2 or nums[0] == 3 or nums[1] == 2 or nums[1] == 3:
+    return True
+  else:
+    return False
+
