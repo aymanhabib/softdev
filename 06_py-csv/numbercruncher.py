1cc748cb3b46c5a7bb0f771fa54f5bc98ab71980
aymanhabib <aymanhabib005@gmail.com>
2022-09-30 09:18:05 -0400

    committed

--- 06_py-csv/numbercruncher.py
+++ 06_py-csv/numbercruncher.py
@@ -9,6 +9,7 @@ OPS:
 
 """
 
+"""
 import csv
 import random
 occu = {}
@@ -18,3 +19,22 @@ with open('occupations.csv') as csvfile:
         occu[row[0]] = float(row[1])
 var = random.choices(list(occu.keys()), list(occu.values()))
 print(var)
+"""
+import random 
+
+with open('occupations.csv') as f:
+    text = f.read()
+ 
+lines = text.split('\n')
+occudict = {}
+for x in lines[1:-1]:
+    pieces = x.split(',')
+    occudict[",".join(pieces[0:-1])] = float(pieces[-1])
+
+nums = []
+for key in list(occudict.keys()):
+    count = int(occudict[key] * 10)
+    for i in range(count):
+        nums.append(key)
+    
+print(random.choice(nums))
