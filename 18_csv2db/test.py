7ef5fa4b3505766056af4295c52b77ca5d1012db
aymanhabib <aymanhabib005@gmail.com>
2022-10-26 09:08:34 -0400

    added

---
+++ 18_csv2db/test.py
@@ -0,0 +1,9 @@
+import csv
+
+with open("students.csv","r") as filename:
+    dictreader = csv.reader(filename)
+    for row in dictreader:
+        for i in range(len(row)):
+            if row[i].isnumeric():
+                row[i]= int(row[i])
+        print(tuple(row))
