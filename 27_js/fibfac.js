fd3d350ff2734193b97a2bc5f86e89b28132753e
Ayman Habib <aymanhabib@Aymans-MacBook-Pro.local>
2023-04-04 14:32:00 -0400

    yurs

---
+++ 27_js/fibfac.js
@@ -0,0 +1,31 @@
+
+// Team Turing Testers :: Ayman Habib 
+// SoftDev pd8
+// K27 -- Basic functions in JavaScript
+// 2023-04-04t
+// --------------------------------------------------
+
+
+// as a duo...
+// pair programming style,
+// implement a fact and fib fxn
+
+
+//Do whatever you think is needed. Think: S I M P L E.   Think: S M A R T
+function fact (n) {
+    if (n < 2) {
+        return 1
+    }
+    return n * fact(n - 1)
+}
+
+function fib (n) {
+    if (n < 1) {
+        return 0
+    }
+    if (n <= 2) {
+        return 1
+    }
+    return fib(n - 1) + fib(n - 2)
+}
+
