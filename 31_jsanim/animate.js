8e1f917ba5f9addcd72bb55b32a0d98ef5400ea4
Ayman Habib <aymanhabib@Aymans-MacBook-Pro.local>
2023-04-25 21:07:08 -0400

    animate

--- 31_jsanim/animate.js
+++ 31_jsanim/animate.js
@@ -4,22 +4,31 @@ var stopButton = document.getElementById("buttonStop");
 
 var ctx = c.getContext("2d");
 
+ctx.strokeStyle = "black"
 ctx.fillStyle = "pink";
 
-var requestID; // init global car for use with animation frames
-
-var clear = (e) => {
-    ctx.clearRect(0, 0, c.width, c.height);
-};
+var requestID; // init global var for use with animation frames
 
 var radius = 0;
 var growing = true;
 
 var drawDot = () => {
-    clear(e);
+
+    if (requestID) {
+        window.cancelAnimationFrame(requestID)
+        console.log("Cancelled")
+    }
+
+    //wipe the canvas
+    ctx.clearRect(0, 0, c.width, c.height);
+
+    //repaint the circle
+    console.log("drawing...")
+    console.log("radius: " + radius)
+    ctx.beginPath();
     ctx.arc(c.width/2, c.height/2, radius, 0, 2 * Math.PI);
-    ctx.fill();
     ctx.stroke();
+    ctx.fill();
 
     if (growing) {
         radius++;
@@ -27,45 +36,37 @@ var drawDot = () => {
     else {
         radius--;
     };
+
     if (radius == c.width/2) {
         growing = false;
+        console.log("growing set to false")
     }
     if (radius == 0) {
         growing = true;
+        console.log("growing set to true")
     };
 
-    window.cancelAnimationFrame();
+    
+    requestID = window.requestAnimationFrame(drawDot);
+
     /*
-    wipe the canvas,
-    repaint the circle,
 
     ...and somewhere (when/where is the right time?)
-    Updat requestID to propagate the animation/
+    Update requestID to propagate the animation/
     You will need
     window.cancelAnimationFrame()
     window.requestAnimationFrame()
     */
 };
 
-var growDot = () => {
-    clear(e);
-    ctx.arc(c.width/2, c.height/2, radius, 0, 2 * Math.PI);
-    ctx.fill();
-    ctx.stroke();
-    radius++;
-};
-
-var shrinkDot = () => {
-
-}
-//var stopIt = function() {
 var stopIt = () => {
     console.log("stopIt invoked...");
-    console.log(requestID);
     /* YOUR CODE HERE
         ...to stop the animation
         You will need window.cancelAnimationFrame()
     */
+    window.cancelAnimationFrame(requestID)
+    console.log("cancelID: " + requestID)
 };
 
 dotButton.addEventListener("click", drawDot);
